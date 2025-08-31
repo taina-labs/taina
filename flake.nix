9ec87b428469aee6ebf026ad29b8db42b1ac6bc5
Zoey Pessanha <zoey.spessanha@zeetech.io>
2025-08-31 01:37:57 -0300

    ci: config

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05-small";
+    elixir-overlay.url = "github:zoedsoupe/elixir-overlay";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    elixir-overlay,
+  }: let
+    inherit (nixpkgs.lib) genAttrs;
+    inherit (nixpkgs.lib.systems) flakeExposed;
+
+    forAllSystems = f:
+      genAttrs flakeExposed (
+        system: let
+          overlays = [elixir-overlay.overlays.default];
+          pkgs = import nixpkgs {inherit system overlays;};
+        in
+          f pkgs
+      );
+  in {
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        name = "taina-dev";
+        packages = with pkgs; [
+          (elixir-with-otp erlang_28)."1.18.4"
+          erlang_28
+          just
+          postgresql
+        ];
+      };
+    });
+  };
+}
